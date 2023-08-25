eea84988829d32a592ac1297cde59ae0342e69b4
Giovana Amorim Campos ♥♥♥ <126572207+giamorim21@users.noreply.github.com>
2023-08-25 12:23:30 -0300

    Update ponteiro.c

--- ponteiro.c
+++ ponteiro.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//int main(){
-//int i, t, M[3][4];
-//for(t = 0; t < 3; ++t){
-//    for(i = 0; i < 4; ++i){
-//        M[t][i] = (t*4) + i +1;
-//    }
-//}
-//printf("\n");
-//for(t = 0; t < 3; ++t){
-//    for(i = 0; i < 4; ++i){
-//        printf("[%d, %d] = %d\n", t, i, M[t][i]);
-//    }
-//}
-//printf("\n");
-//return(0);
-//}
+int main(){
+int i, t, M[3][4];
+for(t = 0; t < 3; ++t){
+    for(i = 0; i < 4; ++i){
+        M[t][i] = (t*4) + i +1;
+    }
+}
+printf("\n");
+for(t = 0; t < 3; ++t){
+    for(i = 0; i < 4; ++i){
+        printf("[%d, %d] = %d\n", t, i, M[t][i]);
+    }
+}
+printf("\n");
+return(0);
+}
 
-//int main(){
-//    int a = 5, b = 6, c = 7;
-//    int v [10] = {0,10,20,30,40,50,60,70,80,90};
-//    int *pt1, *pt2, *pt3;
-//
-//    pt1 = &a;
-//   pt2 = &b;
-//    pt3 = &c;
-//    pt1 = pt2;
-//    pt3 = pt2;
-//    *pt2 = *pt3 + 1000;
-//    pt1 = v;
-//    pt1 = pt1 +1;
-//    for(int k = 0; k < 2; ++k){
-//        *pt1 = *pt1/2;
-//        pt1 = pt1 + 4;
-//    }
+int main(){
+    int a = 5, b = 6, c = 7;
+    int v [10] = {0,10,20,30,40,50,60,70,80,90};
+    int *pt1, *pt2, *pt3;
 
-//    printf("%d, %d, %d\n",a,b,c);
-//    for (int k = 0; k < 10; k++){
-//        printf("vetor[%d] = %d\n", k, v[k]);
-//    }
+    pt1 = &a;
+   pt2 = &b;
+    pt3 = &c;
+    pt1 = pt2;
+    pt3 = pt2;
+    *pt2 = *pt3 + 1000;
+    pt1 = v;
+    pt1 = pt1 +1;
+    for(int k = 0; k < 2; ++k){
+        *pt1 = *pt1/2;
+        pt1 = pt1 + 4;
+    }
 
-//}
+    printf("%d, %d, %d\n",a,b,c);
+    for (int k = 0; k < 10; k++){
+        printf("vetor[%d] = %d\n", k, v[k]);
+   }
+
+}
 
